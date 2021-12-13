0cc8c8bce03504188b649bec5eb62a950cfd1320
Arshia Shafiei <arshiashafiei03@gmail.com>
2021-12-13 22:14:10 +0330

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Battery Reminder
+This might not work with older kernels
+
+Tested with Ubuntu Desktop 20.04 LTS
+___
+This simple script will remind you with desktop notification if your battery is at high percentage and plugged in or at low percentage and not plugged in so you know when to plug or unplug your computer for increasing the Battery lifespan in your laptop or for whatever reason you like.
+___
+Add
+```
+*/5 * * * * /YOUR/PATH/TO/battery.sh > /YOUR/PATH/TO/battery.log 2>&1
+```
+in your crontab using `crontab -e` command in terminal
+
+this will run the script every 5 minutes and save the last log in battery.log
+
+
+If the script is not working and
+you are on arch-based distros
+you might need to change BAT0 to BAT1 in Battery_Percentage and Battery_Status in the script
---
+++ battery.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Change these two to whatever you like
+Battery_low=20
+Battery_high=79
+
+# If the script is not working and
+# you are on arch-based distros
+# you might need to change BAT0 to BAT1 in Battery_Percentage and Battery_Status
+Battery_Percentage=$(cat /sys/class/power_supply/BAT0/capacity)
+Battery_Status=$(cat /sys/class/power_supply/BAT0/status)
+
+export DISPLAY=:0.0
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
+
+if [ $Battery_Percentage -le $Battery_low ] && [ $Battery_Status = Discharging ]; then
+    echo "Plug your power"
+    notify-send "Battery limit reached" "You might want to unplug your Computer"
+elif [ $Battery_Percentage -ge $Battery_high ] && [ $Battery_Status != Discharging ]; then
+    echo "unplug your power"
+    notify-send "Battery Low" "You might want to plug in your Computer"
+fi
